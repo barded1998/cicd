5c22dfba73829521558f1db66a1bf0355723989e
barded1998 <barded1998@gmail.com>
2023-08-12 21:31:16 +0900

    Initail Commit

--- src/main/java/barded/cicd/HelloController.java
+++ src/main/java/barded/cicd/HelloController.java
@@ -1,13 +1,16 @@
 package barded.cicd;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
+        log.info("hello world!!!!!!!!");
         return "hello world!";
     }
 }
