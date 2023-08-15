c73802f0f5be161caab8a712e2aa98a50a7468b5
barded1998 <barded1998@gmail.com>
2023-08-15 14:09:59 +0900

    Updated

--- src/main/java/barded/cicd/HelloController.java
+++ src/main/java/barded/cicd/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        log.info("updated!!!!!");
+        log.info("push!!!!!!");
         return "hello world!";
     }
 }
